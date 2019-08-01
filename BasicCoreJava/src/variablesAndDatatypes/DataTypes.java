e3e952f3164a9bf94978d06e9554bb22deea9905
Vinod <vinodkanchankoti9@gmail.com>
2019-08-01 21:32:08 +0530

    committing My first java code

---
+++ BasicCoreJava/src/variablesAndDatatypes/DataTypes.java
@@ -0,0 +1,39 @@
+package variablesAndDatatypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//integer
+		int age=30;
+		System.out.println(age);
+		
+		//Character
+		char gender='M';
+	   System.out.println(gender);
+	   
+	   //Decimal values
+	   double markes=7.4;
+	   
+	   
+	   //Boolean values
+	   boolean status=true;
+	   
+	   
+	   //Strings
+	   String name="Vinod";
+	   
+	   
+	   //for integer values
+	   byte a=10;
+		/*
+		 * short; int; long;
+		 * 
+		 * 
+		 * //for decimal float; double;
+		 */
+
+	}
+
+}
---
+++ BasicCoreJava/src/variablesAndDatatypes/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package variablesAndDatatypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
